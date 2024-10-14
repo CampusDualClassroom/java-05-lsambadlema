c0cc8db0382d4f0e08c3b51f2e383c24767b989a
Lorena Sambad Lema <lsambadlema@gmail.com>
2024-10-14 16:02:24 +0200

    Terminado Exercise05Test

--- src/main/java/com/campusdual/classroom/Exercise05.java
+++ src/main/java/com/campusdual/classroom/Exercise05.java
@@ -3,26 +3,55 @@ package com.campusdual.classroom;
 public class Exercise05 {
     public static void main(String[] args) {
 
-    }
-
     //TODO ↓
     // Si el valor es menor, tiene que devolver el siguiente mensaje: X es menor que Y
     // Si el valor es mayor o igual, tiene que devolver el siguiente mensaje: X es mayor o igual que Y
-    public static void checkLessOrGreaterEqual(int n1, int n2) {
 
+    checkLessOrGreaterEqual(2, 6);
+    checkLessOrGreaterEqual(6, 6);
+    checkLessOrGreaterEqual(8, 3);
+
+
+    checkMultiply(10, 5);            // Debería indicar que 10 es múltiplo de 5
+    checkMultiply(10, 3);
+
+    checkPositiveNegative (5);
+    checkPositiveNegative (0);
+    checkPositiveNegative (-3);
+
+    }
+
+
+    public static void checkLessOrGreaterEqual(int n1, int n2) {
+        if (n1< n2) {
+            System.out.println(n1 + " es menor que " +n2);
+
+        } else {
+            System.out.println(n1 + " es mayor o igual que " + n2);
+        }
     }
 
     //TODO ↓
     // Si es múltiplo, tiene que devolver el siguiente mensaje: X es múltiplo de Y
     // Si no es múltiplo, tiene que devolver el siguiente mensaje: X no es múltiplo de Y
     public static void checkMultiply(int n1, int n2) {
+        if (n2 !=0 && n1 % n2 == 0) {
+            System.out.println(n1 + " es múltiplo de  " + n2);
 
+        } else {
+            System.out.println(n1 + " no es múltiplo de " + n2);
+        }
     }
 
     //TODO ↓
     // Si es negativo, tiene que devolver el siguiente mensaje: X es negativo.
     // Si es positivo o cero, tiene que devolver el siguiente mensaje: X es positivo.
     public static void checkPositiveNegative(int num) {
+        if (num <0) {
+            System.out.println(num + " es negativo. ");
+        }else {
+            System.out.println(num + " es positivo.");
+        }
         
     }
 }
